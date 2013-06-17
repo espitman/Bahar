80f1dbaa582de43ddeebd92b035c992732dd8eec
Saeed Heidari <saeed.heidari86@gmail.com>
2013-06-17 18:11:25 +0430

    0.0.009
    jsonp features test

--- js/bahar.js
+++ js/bahar.js
@@ -6,19 +6,24 @@ function localJsonpCallback(data) {
 	for (var x in data["data"]) {
 		$("#ppp").append(data["data"][x]["category"] + "<br/>");
 	}
+	$.mobile.hidePageLoadingMsg();
 }
 
 var URL = "http://baharnewspaper.com/app/index.php";
 $(document).on('pageinit', '#home', function() {
-	(function($) {
-		var url = URL;
-		$.ajax({
-			type : 'GET',
-			url : url,
-			async : false,
-			contentType : "application/json",
-			dataType : 'jsonp'
-		});
-	})(jQuery);
+	var url = URL;
+	$.ajax({
+		type : 'GET',
+		data : {
+			callback : 'localJsonpCallback'
+		},
+		beforeSend : function() {
+			$.mobile.showPageLoadingMsg();
+		},
+		url : url,
+		async : false,
+		contentType : "application/json",
+		dataType : 'jsonp'
+	});
 
-});
+}); 
