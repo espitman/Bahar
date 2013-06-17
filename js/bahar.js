4dfe31033abdb474cb4034cb9d3768dd1b5bdbcf
Saeed Heidari <saeed.heidari86@gmail.com>
2013-06-17 17:56:08 +0430

    0.0.008
    jsonp

--- js/bahar.js
+++ js/bahar.js
@@ -1,22 +1,24 @@
-$( document ).bind( "mobileinit", function() {
-  $.mobile.allowCrossDomainPages = true;
+$(document).bind("mobileinit", function() {
+	$.mobile.allowCrossDomainPages = true;
 });
 
 function localJsonpCallback(data) {
-	for(var x in data["data"]) {
-		$("#ppp").append(data["data"][x]["category"]+"<br/>");
+	for (var x in data["data"]) {
+		$("#ppp").append(data["data"][x]["category"] + "<br/>");
 	}
 }
+
 var URL = "http://baharnewspaper.com/app/index.php";
 $(document).on('pageinit', '#home', function() {
-	$.ajax({
-		url : URL,
-		dataType : "jsonp",
-		type : 'post',
-		processData : false,
-		crossDomain : true,
-		contentType : "application/json",
-	});
+	(function($) {
+		var url = URL;
+		$.ajax({
+			type : 'GET',
+			url : url,
+			async : false,
+			contentType : "application/json",
+			dataType : 'jsonp'
+		});
+	})(jQuery);
 
-
-}); 
+});
