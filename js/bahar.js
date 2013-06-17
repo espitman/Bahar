62bec36074e19993a8af7fe5e3ec05b205f169e7
Saeed Heidari <saeed.heidari86@gmail.com>
2013-06-17 18:58:46 +0430

    0.0.010
    + Show last day pages title
    + Page loader

--- js/bahar.js
+++ js/bahar.js
@@ -1,21 +1,26 @@
+var URL = "http://baharnewspaper.com/app/index.php";
+
 $(document).bind("mobileinit", function() {
 	$.mobile.allowCrossDomainPages = true;
 });
 
-function localJsonpCallback(data) {
-	for (var x in data["data"]) {
-		$("#ppp").append(data["data"][x]["category"] + "<br/>");
+//-----------------------------------------------------------
+
+function first_page_callback(data) {
+	for (var x in data) {
+		$("#profiles").append("<li data-no='" + data[x]["fld_Profile_No"] + "'>" + data[x]["fld_Year"] + "/" + data[x]["fld_Month"] + "/" + data[x]["fld_Day"] + "</li>");
 	}
 	$.mobile.hidePageLoadingMsg();
 }
 
-var URL = "http://baharnewspaper.com/app/index.php";
+
 $(document).on('pageinit', '#home', function() {
 	var url = URL;
 	$.ajax({
 		type : 'GET',
 		data : {
-			callback : 'localJsonpCallback'
+			f : "first_page",
+			callback : 'first_page_callback'
 		},
 		beforeSend : function() {
 			$.mobile.showPageLoadingMsg();
@@ -25,5 +30,30 @@ $(document).on('pageinit', '#home', function() {
 		contentType : "application/json",
 		dataType : 'jsonp'
 	});
+});
+//-----------------------------------------------------------
+$(document).on("click", "#home ul li", function() {
+	$.mobile.showPageLoadingMsg();
+	var url = URL;
+	$.ajax({
+		type : 'GET',
+		data : {
+			f : "pages_page",
+			callback : 'show_pages'
+		},
+		url : url,
+		async : false,
+		contentType : "application/json",
+		dataType : 'jsonp'
+	});
 
-}); 
+});
+
+function show_pages(data) {
+	for (var x in data["data"]) {
+		$("#pages-ul").append("<li>"+data["data"][x]["category"] + "</li>");
+	}
+	$.mobile.changePage("#pages", {transition: "slide"});
+	$.mobile.hidePageLoadingMsg();
+}
+
