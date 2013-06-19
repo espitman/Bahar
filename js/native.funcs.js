350b9c5e3b7c67fae819ac5b86017da7e428d25b
Saeed Heidari <saeed.heidari86@gmail.com>
2013-06-19 15:59:09 +0430

    0.0.326 - Download file worked

--- js/native.funcs.js
+++ js/native.funcs.js
@@ -1,47 +1,22 @@
-function onBodyLoad()
-{
-	//document.addEventListener("deviceready", onDeviceReady, false);
-	downloadFile();
-	
+function downloadFile() {
+	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function onFileSystemSuccess(fileSystem) {
+		fileSystem.root.getFile("dummy.html", {
+			create : true,
+			exclusive : false
+		}, function gotFileEntry(fileEntry) {
+			var sPath = fileEntry.fullPath.replace("dummy.html", "");
+			var fileTransfer = new FileTransfer();
+			fileEntry.remove();
+
+			fileTransfer.download("http://www.baharnewspaper.com/Images/Logo/bahar.jpg", sPath + "bahar.jpg", function(theFile) {
+				alert(">>" + theFile.toURI());
+			}, function(error) {
+				console.log("download error source " + error.source);
+				console.log("download error target " + error.target);
+				console.log("upload error code: " + error.code);
+			});
+		}, fail);
+	}, fail);
+
 }
- 
-    function downloadFile(){
-        window.requestFileSystem(
-                     LocalFileSystem.PERSISTENT, 0, 
-                     function onFileSystemSuccess(fileSystem) {
-                     fileSystem.root.getFile(
-                                 "dummy.html", {create: true, exclusive: false}, 
-                                 function gotFileEntry(fileEntry){
-                                 var sPath = fileEntry.fullPath.replace("dummy.html","");
-                                 var fileTransfer = new FileTransfer();
-                                 fileEntry.remove();
- 
-                                 fileTransfer.download(
-                                           "http://www.baharnewspaper.com/Images/Logo/bahar.jpg",
-                                           sPath + "bahar.jpg",
-                                           function(theFile) {
-                                           alert(">>"+theFile.toURI());
-                                           },
-                                           function(error) {
-                                           console.log("download error source " + error.source);
-                                           console.log("download error target " + error.target);
-                                           console.log("upload error code: " + error.code);
-                                           }
-                                           );
-                                 }, 
-                                 fail);
-                     }, 
-                     fail);
- 
-    }
- 
- 
- 
-    function fail(evt) {
-        console.log(evt.target.error.code);
-    }
- 
-function onDeviceReady()
-{
-        downloadFile();
-}
+
