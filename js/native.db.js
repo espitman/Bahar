f7c03748c1339224a76ddfe1740bdcf564244cbf
Saeed Heidari <saeed.heidari86@gmail.com>
2013-06-21 15:51:04 +0430

    0.0.425

--- js/native.db.js
+++ js/native.db.js
@@ -1,8 +1,8 @@
 function populateDB(tx) {
-	tx.executeSql('DROP TABLE IF EXISTS DEMO');
+	//tx.executeSql('DROP TABLE IF EXISTS DEMO');
 	tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id unique, data)');
-	tx.executeSql('INSERT INTO DEMO (id, data) VALUES (1, "First row")');
-	tx.executeSql('INSERT INTO DEMO (id, data) VALUES (2, "Second row")');
+	tx.executeSql('INSERT INTO DEMO (id, data) VALUES (3, "3 row")');
+	tx.executeSql('INSERT INTO DEMO (id, data) VALUES (4, "4 row")');
 }
 
 // Query the database
