8c246789fed49ca14938bb19289d510cd387353f
Saeed Heidari <saeed.heidari86@gmail.com>
2013-06-22 11:04:41 +0430

    0.0.528

--- js/native.db.js
+++ js/native.db.js
@@ -166,10 +166,8 @@ function insertProfile(profileData) {
 }
 
 function doInsertProfile(tx, profileData) {
-	console.log(profileData);
-	tx.executeSql('DROP TABLE IF EXISTS profiles');
+	//tx.executeSql('DROP TABLE IF EXISTS profiles');
 	tx.executeSql('CREATE TABLE IF NOT EXISTS profiles (id unique, prfNo, date)');
 	var sql = 'INSERT INTO profiles (prfNo,date) VALUES ("' + profileData["prfNo"] + '","' + profileData["date"] + '")';
-	console.log(sql);
 	tx.executeSql(sql);
 }
